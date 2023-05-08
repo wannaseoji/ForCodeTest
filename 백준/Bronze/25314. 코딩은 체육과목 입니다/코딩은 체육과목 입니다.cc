c0f65326a8e13ce0e0602dd4d32efe8046c3180a
wannaseoji <91694379+wannaseoji@users.noreply.github.com>
2023-05-09 03:13:33 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int N, sum=0;
+	cin >> N;
+	sum = N / 4;
+	for (int i = 0; i < sum; i++) {
+		
+		cout << "long ";
+	
+	}
+	cout << "int";
+	return 0;
+}
