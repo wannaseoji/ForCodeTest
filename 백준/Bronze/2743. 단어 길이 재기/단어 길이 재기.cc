763a6541504edcd036204c061f537bf332c4824b
wannaseoji <91694379+wannaseoji@users.noreply.github.com>
2023-05-21 02:12:50 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string arr;
+    cin >> arr;
+    int i=0;
+    while(arr[i]!='\0'){
+        i++;
+    }
+    cout << i ;
+    return 0;
+    
+}
