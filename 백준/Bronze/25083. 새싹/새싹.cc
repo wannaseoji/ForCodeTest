897f33b66576e7c7c43f41f80aa7699bb9592e93
wannaseoji <91694379+wannaseoji@users.noreply.github.com>
2023-06-02 18:15:27 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+ 
+using namespace std;
+ 
+int main(int argc, char const *argv[]) {
+ 
+	string s = "         ,r'\"7\n";   
+	       s += "r`-_   ,'  ,/\n";    
+	       s += " \\. \". L_r'\n";    
+	       s += "   `~\\/\n";        
+	       s += "      |\n";           
+	       s += "      |";
+	cout << s;
+	return 0;
+}
