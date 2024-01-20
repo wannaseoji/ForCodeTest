6a7ea4462c446a715b935b2e3db0525b852ab8c4
Jiwan Seo <91694379+wannaseoji@users.noreply.github.com>
2024-01-20 23:43:07 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10812 ms, Memory: 305740 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -1,60 +1,52 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static int N, M;
-    public static ArrayList<Integer>[] relations;
-    public static int[] hackingCnt;
-    public static boolean[] visited;
+    static int N, M;
+    static ArrayList<Integer>[] relations;
+    static int[] hackingCnt;
+    static boolean[] visited;
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        relations = new ArrayList[N+1];
-        for(int i=1;i<=N;i++){
+        relations = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
             relations[i] = new ArrayList<>();
         }
 
-        for(int i=0;i<M;i++){
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            relations[A].add(B);
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
+            relations[n1].add(n2);
         }
 
-        hackingCnt = new int[N+1];
-        for(int i=1;i<=N;i++){
-            visited = new boolean[N+1];
+        hackingCnt = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            visited = new boolean[N + 1];
             countHacking(i);
         }
 
         ArrayList<Integer> maxIndex = new ArrayList<>();
         int max = Integer.MIN_VALUE;
-        for(int i=1;i<=N;i++){
-            if(hackingCnt[i] > max){
+        for (int i = 1; i <= N; i++) {
+            if (hackingCnt[i] > max) {
                 maxIndex.clear();
-
                 max = hackingCnt[i];
                 maxIndex.add(i);
-            }
-            else if(hackingCnt[i] == max){
+            } else if (hackingCnt[i] == max) {
                 maxIndex.add(i);
             }
         }
 
         StringBuilder ans = new StringBuilder();
-        for(int idx:maxIndex){
+        for (int idx : maxIndex) {
             ans.append(idx).append(" ");
         }
 
@@ -65,16 +57,16 @@ public class Main {
         bw.close();
     }
 
-    public static void countHacking(int num){
+    public static void countHacking(int num) {
         Queue<Integer> queue = new ArrayDeque<>();
         queue.add(num);
         visited[num] = true;
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             int now = queue.poll();
 
-            for(int next:relations[now]){
-                if(visited[next]){
+            for (int next : relations[now]) {
+                if (visited[next]) {
                     continue;
                 }
 
